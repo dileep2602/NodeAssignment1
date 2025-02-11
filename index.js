1585ba34917bbe1184c311b3c0f8eefbfe29332c
Dileep Kumar <dileepkharvi037gmail.com>
2025-02-11 12:32:29 +0530

    created file using fs module

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//create a promoise to display a mesaage and cal the promise.
 function displayMessage() {
     return new Promise((resolve, reject) => {
         setTimeout(()=>{
@@ -9,3 +10,18 @@ function displayMessage() {
 displayMessage()
     .then(message => console.log(message))
     .catch(error => console.log("Error : ",error));
+
+// Create a file using fs module and async/await.
+const fs = require("fs").promises;
+
+async function createFile() {
+    try {
+        const content = "this file is created using fs module";
+        await fs.writeFile("message.txt",content);
+        console.log("File created successfully");
+    } catch (error) {
+        console.error(" Error while creating file : ",error);
+    }
+}
+
+createFile();
